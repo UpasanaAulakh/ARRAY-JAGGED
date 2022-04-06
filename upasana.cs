7559d050a8c8573f753c18813fed324b0ab2551b
UpasanaAulakh <101379151+UpasanaAulakh@users.noreply.github.com>
2022-04-06 18:39:35 +0530

    Add files via upload

---
+++ upasana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JAGGEDARR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[2][];
+            arr[0] = new int[] { 11, 23, 33, 67 };
+            arr[1] = new int[] { 45, 56, 88, 99, 23 };
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=0; j<arr.Length; j++)
+                {
+                    Console.WriteLine(arr[i][j] + " ");
+                    
+                }
+                Console.WriteLine();
+
+            }
+               
+            
+        }
+    }
+}
